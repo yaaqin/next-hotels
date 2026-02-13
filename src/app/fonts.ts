f2f80d2f85c5e8e8f66095347510f1d501ea7395
Yaaqin <yaqin.me@gmail.com>
2026-02-13 11:03:55 +0700

    SETUPS : font setups

---
+++ src/app/fonts.ts
@@ -0,0 +1,7 @@
+import { Saira } from "next/font/google";
+
+export const saira = Saira({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Homepage() {
   return (
-    <div>
-      homepage here
+    <div className='w-full h-screen flex text-center justify-center items-center'>
+      <h1 className='capitalize text-5xl'>homepage here</h1>
     </div>
   )
 }
