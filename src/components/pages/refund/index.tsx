94f65cc05a8d6185dedc3b27651abe1be8baff44
Yaaqin <yaqin.me@gmail.com>
2026-03-16 14:42:05 +0700

    FIX : [REFUND] fix error build

--- src/components/pages/refund/index.tsx
+++ src/components/pages/refund/index.tsx
@@ -1,24 +1,9 @@
 'use client'
 import { useRefundList } from '@/src/hooks/query/refund/list'
 import RefundTable from '../../organisms/refund/table'
-import { refundListState } from '@/src/models/refund/list'
-import { useApproveRefund } from '@/src/hooks/mutation/refund/approve'
 
 export default function RefundPage() {
   const { data } = useRefundList()
-
-  const { mutate: approve, isPending: isApproving } = useApproveRefund()
-
-  const handleApprove = (refund: refundListState) => {
-    approve(refund.booking.bookingCode, {
-      onSuccess: () => {
-        setSelectedRefund(null)
-      },
-      onError: () => {
-        alert('Gagal approve refund, silakan coba lagi')
-      },
-    })
-  }
   return (
     <RefundTable data={data?.data?.refunds ?? []} />
   )
