fd7c44c7f730928f34e9ecb53b9967f83488226d
Yaaqin <yaqin.me@gmail.com>
2026-06-25 14:58:29 +0700

    UPDATE : [HOME] disable selector checkin

--- src/components/pages/(publicPage)/home/bookingPage2.tsx
+++ src/components/pages/(publicPage)/home/bookingPage2.tsx
@@ -76,7 +76,7 @@ export default function BookingPage2() {
         </motion.section>
       </section>
 
-      <HotelBookingSelector />
+      {/* <HotelBookingSelector /> */}
       <WhoWeAre />
 
       {/* Text Section */}
